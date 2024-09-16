ab5e6d38eac98b0183315e5431ed7bf58d0a000e
otmanedouieb <douieb197@gmail.com>
2024-09-17 00:03:43 +0100

    Use && short conditional rendering

--- src/index.js
+++ src/index.js
@@ -86,12 +86,23 @@ function Menu() {
 
 // Component footer
 function Footer() {
-  const hour = new Date().getHours();
+  const hour = 13; //new Date().getHours();
   const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
 
-  return <footer className="footer">we're currently open</footer>;
+  return (
+    <footer className="footer">
+      {isOpen && (
+        <div>
+          <p>
+            We're open until {closeHour}:00. Come visit us or order online.{" "}
+          </p>
+          <button className="btn">Order</button>
+        </div>
+      )}
+    </footer>
+  );
 }
 
 // Create App component
